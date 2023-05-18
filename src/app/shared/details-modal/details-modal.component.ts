170e9ea342979111d64452e7c7b3721855eec0f0
Allan <j.a.o.huthan@gmail.com>
2023-05-18 14:40:42 -0300

    modal component created

---
+++ src/app/shared/details-modal/details-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details-modal',
+  templateUrl: './details-modal.component.html',
+  styleUrls: ['./details-modal.component.less']
+})
+export class DetailsModalComponent {
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AlertModalComponent } from './alert-modal/alert-modal.component';
 import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
+import { DetailsModalComponent } from './details-modal/details-modal.component';
 
 @NgModule({
-  declarations: [AlertModalComponent, PokemonCardComponent],
+  declarations: [AlertModalComponent, PokemonCardComponent, DetailsModalComponent],
   imports: [CommonModule],
   exports: [AlertModalComponent, PokemonCardComponent],
 })
