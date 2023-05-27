4f523a4af0a7511801fa50b39ac4b7d45f20f56b
Allan Oliveira <j.a.o.huthan@gmail.com>
2023-05-27 08:20:49 -0300

    button component implemented

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,14 +3,21 @@ import { CommonModule } from '@angular/common';
 import { AlertModalComponent } from './components/alert-modal/alert-modal.component';
 import { PokemonCardComponent } from './components/pokemon-card/pokemon-card.component';
 import { DetailsModalComponent } from './components/details-modal/details-modal.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
     AlertModalComponent,
     PokemonCardComponent,
     DetailsModalComponent,
+    ButtonComponent,
   ],
   imports: [CommonModule],
-  exports: [AlertModalComponent, PokemonCardComponent, DetailsModalComponent],
+  exports: [
+    AlertModalComponent,
+    PokemonCardComponent,
+    DetailsModalComponent,
+    ButtonComponent,
+  ],
 })
 export class SharedModule {}
